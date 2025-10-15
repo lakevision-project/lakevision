9bd5b21ae6b7a11417417d5d0aa9def4a34f3cdc
codrut <codrut.stoicescu@yahoo.com>
2025-10-15 16:09:55 +0300

    update wording

--- fe/src/lib/components/Chat.svelte
+++ fe/src/lib/components/Chat.svelte
@@ -358,7 +358,7 @@
 <div class="chat-container">
     <div class="status">{status}</div>
     <div class="details">
-        <p>You can ask questions like:</p>
+        <p>You can find out information about you lakehouse like:</p>
         <ul>
             <li>List namespaces</li>
             <li>List tables in namespace X</li>
