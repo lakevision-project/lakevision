88cad2460a1ccf9c7feadcf75ca893d140ddcac3
codrut <codrut.stoicescu@yahoo.com>
2025-10-21 21:22:17 +0300

    fix worker script

--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@ cd /app/be/app/
 fastapi run /app/be/app/api.py --port 8000 &
 cd /app/be
 python3 -m app.scheduler &
-python3 -m app.worker &
 #gunicorn -k uvicorn.workers.UvicornWorker api:app --bind 0.0.0.0:8000 --workers 2 --threads 32
 cd /app/fe
 npm run build
--- worker.sh
+++ worker.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 cd /app/be
-python3 -m app.worker &
+python3 -m app.worker
